89dca53fcdd5fee6158f0df9935d629292faed91
ino-aki <huanghongjingshang043@gmail.com>
2024-06-16 13:32:33 +0900

    商品情報をDBに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :description, :category_id, :itemcondition_id, :shippingfee_id, :prefecture_id, :shippingday_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one_attached :image
+  belongs_to :category
+  belongs_to :itemcondition
+  belongs_to :shippingfee
+  belongs_to :prefecture
+  belongs_to :shippingday
+  
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :itemcondition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shippingfee_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shippingday_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true
 end
